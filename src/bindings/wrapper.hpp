182773854a33671e77dcc78378292dcf170f61ca
Anders Smedegaard Pedersen <anders@smedegaard.io>
2024-10-29 22:52:29 +0100

    'yoink!'

--- src/bindings/wrapper.hpp
+++ src/bindings/wrapper.hpp
@@ -1,17 +1,3 @@
 #pragma once
 
 #include <hip/hip_runtime.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Initialize HIP and check for errors
-hipError_t hip_initialize();
-
-// Get number of available GPU devices
-hipError_t hip_get_device_count(int* count);
-
-#ifdef __cplusplus
-}
-#endif
