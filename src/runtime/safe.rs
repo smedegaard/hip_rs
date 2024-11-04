2dfd8fe19974c5b82039e98deda87d973e9c0b8b
Anders Smedegaard Pedersen <anders.smedegaard.pedersen@gsilo.ai>
2024-11-04 09:22:47 +0000

    fix mutability

--- src/runtime/safe.rs
+++ src/runtime/safe.rs
@@ -203,9 +203,9 @@ pub fn get_device_name(device: Device) -> Result<String> {
 /// * The runtime is not initialized
 /// * There was an error retrieving the UUID
 pub fn get_device_uuid_bytes(device: Device) -> Result<[i8; 16]> {
-    let mut hip_bytes: sys::hipUUID_t = sys::hipUUID_t { bytes: [0; 16] };
+    let mut hip_bytes = sys::hipUUID_t { bytes: [0; 16] };
     unsafe {
-        let code = sys::hipDeviceGetUuid(*hip_bytes, device.id);
+        let code = sys::hipDeviceGetUuid(&mut hip_bytes, device.id);
         (hip_bytes.bytes, code).to_result()
     }
 }
