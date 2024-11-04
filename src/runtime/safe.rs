4e65ff5c0d139bd1b3e749693f5a590afcdb3d8f
Anders Smedegaard Pedersen <anders@smedegaard.io>
2024-11-04 10:10:57 +0100

    'use hipUUID_t'

--- src/runtime/safe.rs
+++ src/runtime/safe.rs
@@ -203,10 +203,10 @@ pub fn get_device_name(device: Device) -> Result<String> {
 /// * The runtime is not initialized
 /// * There was an error retrieving the UUID
 pub fn get_device_uuid_bytes(device: Device) -> Result<[u8; 16]> {
-    let mut bytes = [0u8; 16];
+    let mut hip_bytes: sys::hipUUID_t = sys::hipUUID_t { bytes: [0; 16] };
     unsafe {
-        let code = sys::hipDeviceGetUuid(&mut bytes, device.id);
-        (bytes, code).to_result()
+        let code = sys::hipDeviceGetUuid(*hip_bytes, device.id);
+        (hip_bytes.bytes, code).to_result()
     }
 }
 
