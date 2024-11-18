7c92ed2c83734579136c69d06155f8f0f6263416
Anders Smedegaard Pedersen <anders@smedegaard.io>
2024-11-18 13:36:36 +0100

    move logic of malloc into MemoryPointer

--- src/types/memory.rs
+++ src/types/memory.rs
@@ -1,3 +1,5 @@
+use super::{HipError, HipResult, Result};
+
 /// A wrapper for device memory allocated on the GPU.
 /// Automatically frees the memory when dropped.
 pub struct MemoryPointer<T> {
@@ -5,12 +7,43 @@ pub struct MemoryPointer<T> {
     size: usize,
 }
 
+impl<T> MemoryPointer<T> {
+    pub fn new(size: usize) -> Result<Self> {
+        // Handle zero size allocation according to spec
+        if size == 0 {
+            return Ok(DevicePointer {
+                ptr: std::ptr::null_mut(),
+                size: 0,
+            });
+        }
+
+        let mut ptr = std::ptr::null_mut();
+
+        let code = unsafe {
+            sys::hipMalloc(
+                &mut ptr as *mut *mut T as *mut *mut std::ffi::c_void,
+                size * std::mem::size_of::<T>(),
+            )
+        };
+
+        let pointer = Self {
+            ptr: ptr as *mut T,
+            size,
+        };
+
+        (pointer, code).to_result()
+    }
+}
+
 // The Drop trait does not return anything by design
 impl<T> Drop for MemoryPointer<T> {
     fn drop(&mut self) {
-        // Safe because we own the pointer and it was allocated with hipMalloc
         unsafe {
-            let _ = sys::hipFree(self.ptr as *mut std::ffi::c_void);
+            let code = sys::hipFree(self.ptr as *mut std::ffi::c_void);
+            if code != 0 {
+                let error = HipError::new(code);
+                log::error!("MemoryPointer failed to free memory: {}", error);
+            }
         }
     }
 }
